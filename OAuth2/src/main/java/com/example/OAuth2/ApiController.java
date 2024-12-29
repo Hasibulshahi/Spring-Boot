bacd003c6afc46248410a88e93e6d5e81ef2ee2b
Hasibul Hassan <shahi_aust@hotmail.com>
2024-12-29 15:29:50 +0600

    update

--- OAuth2/src/main/java/com/example/OAuth2/ApiController.java
+++ OAuth2/src/main/java/com/example/OAuth2/ApiController.java
@@ -1,27 +1,10 @@
 package com.example.OAuth2;
 
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class ApiController {
-    @GetMapping("/oauth")
-    public String greet() {
-        return "Welcome to OAuth";
-    }
-
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal OidcUser principal, Model model) {
-        if (principal != null) {
-            model.addAttribute("user", principal);
-        }
-        return "home"; // Ensure "home.html" exists in `src/main/resources/templates`
-    }
-
-    @GetMapping("/logout")
-    public String logout() {
-        return "redirect:https://dev-1idbplxpxvks61k8.us.auth0.com/v2/logout?client_id=8LxfKyYuH5kTVzQsSHFaRNmYHJvRBmEk&returnTo=http://localhost:8080";
+    public String greet() {
+        return "Welcome to Telusko, telusko.com";
     }
 }
